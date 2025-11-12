08bba57002062b1628c32ca3abaa87efbc96b9ad
Colin454 <blackcolin138@gmail.com>
2025-11-12 11:52:49 +0700

    Initial Commit

---
+++ SJ/SJApp.swift
@@ -0,0 +1,17 @@
+//
+//  SJApp.swift
+//  SJ
+//
+//  Created by colin black on 12/11/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct SJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
